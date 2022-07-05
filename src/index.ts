ee92d14b77371ed73c361a21381edb9331471cc8
francisyzy <github@francisyzy.com>
2022-07-05 22:14:04 +0800

    Rename and delete extra files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 // Node modules.
 import { mkdirp, writeFile } from 'fs-extra';
 // Local modules.
-import { getRaidBosses } from './raid';
-import { getEvents } from './event';
-import { getResearches } from './research';
-import { getEggs } from './egg';
+import { getRaidBosses } from './jacob';
 
 const main = async () => {
   const outputPath = './artifacts';
@@ -19,32 +16,6 @@ const main = async () => {
     console.error(e);
   }
 
-  // Events.
-  try {
-    const events = await getEvents();
-    await writeFile(`${outputPath}/events.json`, JSON.stringify(events, null, 2));
-    await writeFile(`${outputPath}/events.min.json`, JSON.stringify(events));
-  } catch (e) {
-    console.error(e);
-  }
-
-  // Researches (deprecated).
-  // try {
-  //   const researches = await getResearches();
-  //   await writeFile(`${outputPath}/researches.json`, JSON.stringify(researches, null, 2));
-  //   await writeFile(`${outputPath}/researches.min.json`, JSON.stringify(researches));
-  // } catch (e) {
-  //   console.error(e);
-  // }
-
-  // Eggs.
-  try {
-    const eggs = await getEggs();
-    await writeFile(`${outputPath}/eggs.json`, JSON.stringify(eggs, null, 2));
-    await writeFile(`${outputPath}/eggs.min.json`, JSON.stringify(eggs));
-  } catch (e) {
-    console.error(e);
-  }
 };
 
 main();
